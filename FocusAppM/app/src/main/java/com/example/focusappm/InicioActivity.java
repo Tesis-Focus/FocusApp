1a42db132ae03b8619c8fcc106392e737ce3fbce
AnyiBustos <anyipaolabustos@gmail.com>
2020-03-07 15:32:48 -0500

    Pantalla Registro

--- FocusAppM/app/src/main/java/com/example/focusappm/InicioActivity.java
+++ FocusAppM/app/src/main/java/com/example/focusappm/InicioActivity.java
@@ -10,17 +10,27 @@ import android.widget.Button;
 public class InicioActivity extends AppCompatActivity {
 
     Button crearCuenta=null;
+    Button inicioSesion=null;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_inicio);
 
         crearCuenta = (Button) findViewById(R.id.crearCuenta);
+        inicioSesion =(Button) findViewById(R.id.inicioSesion);
 
         crearCuenta.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(getApplicationContext(), RegistroActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        inicioSesion.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(),RegistroActivity.class);
                 startActivity(intent);
             }
         });
--- FocusAppM/app/src/main/java/com/example/focusappm/RegistroActivity.java
+++ FocusAppM/app/src/main/java/com/example/focusappm/RegistroActivity.java
@@ -1,15 +1,28 @@
 package com.example.focusappm;
 
 import androidx.appcompat.app.AppCompatActivity;
-import org.drools.core.*;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class RegistroActivity extends AppCompatActivity {
 
+    Button registro = null;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registro);
+
+        registro = (Button) findViewById(R.id.inicioSesion);
+
+        registro.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent= new Intent (getApplicationContext(),InicioActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
