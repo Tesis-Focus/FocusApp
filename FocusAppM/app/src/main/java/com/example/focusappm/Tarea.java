0973acc0bcd4b9c7c774456978470e1bc922f17a
Lina Marcela Varón <31346699+linamvaron97@users.noreply.github.com>
2020-03-21 17:35:32 -0500

    Clase tarea

---
+++ FocusAppM/app/src/main/java/com/example/focusappm/Tarea.java
@@ -0,0 +1,98 @@
+package com.example.focusappm;
+
+import java.util.Date;
+
+public class Tarea {
+
+    private String nombre;
+    private String descripcion;
+    private String tema;
+    private String complejidad;
+    private String area;
+    private String clasificacion;
+    private Date fechaAsignacion;
+    private Date fechaInicio;
+    private Date fechaFinalizacion;
+    private Date fechaEntrega;
+    //private Actividad actividad;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public String getTema() {
+        return tema;
+    }
+
+    public void setTema(String tema) {
+        this.tema = tema;
+    }
+
+    public String getComplejidad() {
+        return complejidad;
+    }
+
+    public void setComplejidad(String complejidad) {
+        this.complejidad = complejidad;
+    }
+
+    public String getArea() {
+        return area;
+    }
+
+    public void setArea(String area) {
+        this.area = area;
+    }
+
+    public String getClasificacion() {
+        return clasificacion;
+    }
+
+    public void setClasificacion(String clasificacion) {
+        this.clasificacion = clasificacion;
+    }
+
+    public Date getFechaAsignacion() {
+        return fechaAsignacion;
+    }
+
+    public void setFechaAsignacion(Date fechaAsignacion) {
+        this.fechaAsignacion = fechaAsignacion;
+    }
+
+    public Date getFechaInicio() {
+        return fechaInicio;
+    }
+
+    public void setFechaInicio(Date fechaInicio) {
+        this.fechaInicio = fechaInicio;
+    }
+
+    public Date getFechaFinalizacion() {
+        return fechaFinalizacion;
+    }
+
+    public void setFechaFinalizacion(Date fechaFinalizacion) {
+        this.fechaFinalizacion = fechaFinalizacion;
+    }
+
+    public Date getFechaEntrega() {
+        return fechaEntrega;
+    }
+
+    public void setFechaEntrega(Date fechaEntrega) {
+        this.fechaEntrega = fechaEntrega;
+    }
+}
