abc28fbb9931ee673dafc35a8c97b362e19addbf
AnyiBustos <anyipaolabustos@gmail.com>
2020-03-21 13:48:23 -0500

    agregaractividad

---
+++ FocusAppM/app/src/main/java/com/example/focusappm/Actividad.java
@@ -0,0 +1,100 @@
+package com.example.focusappm;
+
+import java.util.Date;
+
+public class Actividad {
+    private String id;
+    private String nombre;
+    private String descripcion;
+    private String tipo;
+    private String motivacion;
+    private String desempeño;
+    private String fechaInicio;
+    private String fechaFinal;
+    private Boolean horarioFijo;
+    private String idUsaurio;
+
+    public Actividad() {
+
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public String getMotivacion() {
+        return motivacion;
+    }
+
+    public void setMotivacion(String motivacion) {
+        this.motivacion = motivacion;
+    }
+
+    public String getDesempeño() {
+        return desempeño;
+    }
+
+    public void setDesempeño(String desempeño) {
+        this.desempeño = desempeño;
+    }
+
+    public String getFechaInicio() {
+        return fechaInicio;
+    }
+
+    public void setFechaInicio(String fechaInicio) {
+        this.fechaInicio = fechaInicio;
+    }
+
+    public String getFechaFinal() {
+        return fechaFinal;
+    }
+
+    public void setFechaFinal(String fechaFinal) {
+        this.fechaFinal = fechaFinal;
+    }
+
+    public Boolean getHorarioFijo() {
+        return horarioFijo;
+    }
+
+    public void setHorarioFijo(Boolean horarioFijo) {
+        this.horarioFijo = horarioFijo;
+    }
+
+    public String getIdUsaurio() {
+        return idUsaurio;
+    }
+
+    public void setIdUsaurio(String idUsaurio) {
+        this.idUsaurio = idUsaurio;
+    }
+}
