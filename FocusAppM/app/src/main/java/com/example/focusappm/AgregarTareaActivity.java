cf397028d666545c58559d0c9c1df53853174eef
Lina Marcela Varón <31346699+linamvaron97@users.noreply.github.com>
2020-03-25 22:26:16 -0500

    agregar tarea

--- FocusAppM/app/src/main/java/com/example/focusappm/AgregarTareaActivity.java
+++ FocusAppM/app/src/main/java/com/example/focusappm/AgregarTareaActivity.java
@@ -1,18 +1,31 @@
 package com.example.focusappm;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.DatePickerDialog;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.Spinner;
+import android.widget.Toast;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.List;
 
 public class AgregarTareaActivity extends AppCompatActivity {
 
@@ -22,10 +35,18 @@ public class AgregarTareaActivity extends AppCompatActivity {
     EditText txtDescripTarea;
     EditText txtTemaTarea;
     Spinner sprComplejidad;
+    Spinner sprClasificacion;
+    Spinner sprArea;
     Spinner sprActividad;
     Button btnGuardarTarea;
     Calendar calendario;
     DatePickerDialog datePickerDialog;
+
+    FirebaseDatabase database;
+    DatabaseReference myRef;
+    FirebaseAuth mAuth;
+    FirebaseUser user;
+    public static final String PATH_TAREAS = "tareas/";
 
 
     @Override
@@ -33,14 +54,22 @@ public class AgregarTareaActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_agregar_tarea);
 
+        database = FirebaseDatabase.getInstance();
+        myRef = FirebaseDatabase.getInstance().getReference();
+        mAuth = FirebaseAuth.getInstance();
+        user = mAuth.getCurrentUser();
+
         txtFechaEntrega = findViewById(R.id.txtFechaEntrega);
         btnFechaEntrega = findViewById(R.id.btnFechaEntrega);
         txtNombTarea = findViewById(R.id.txtNombTarea);
         txtDescripTarea = findViewById(R.id.txtDescripTarea);
         txtTemaTarea = findViewById(R.id.txtTemaTarea);
         sprComplejidad = findViewById(R.id.sprComplejidad);
+        sprClasificacion = findViewById(R.id.sprClasificacion);
+        sprArea = findViewById(R.id.sprArea);
         sprActividad = findViewById(R.id.sprActividad);
         btnGuardarTarea = findViewById(R.id.btnGuardarTarea);
+
 
         btnFechaEntrega.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -65,20 +94,73 @@ public class AgregarTareaActivity extends AppCompatActivity {
         ArrayAdapter<CharSequence> adapterComplej = ArrayAdapter.createFromResource(this, R.array.Complejidad, android.R.layout.simple_spinner_item);
         sprComplejidad.setAdapter(adapterComplej);
 
-        ArrayAdapter<CharSequence> adapterActiv = ArrayAdapter.createFromResource(this, R.array.Bajo_Medio_Alto, android.R.layout.simple_spinner_item);
-        sprActividad.setAdapter(adapterActiv);
+        //ArrayAdapter<CharSequence> adapterClasif = ArrayAdapter.createFromResource(this, R.array.Clasificacion, android.R.layout.simple_spinner_item);
+        //sprClasificacion.setAdapter(adapterClasif);
 
-        /*
+        spinnerActiv();
+
+        //ArrayAdapter<CharSequence> adapterArea = ArrayAdapter.createFromResource(this, R.array.Area, android.R.layout.simple_spinner_item);
+        //sprArea.setAdapter(adapterArea);
+
         btnGuardarTarea.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
+            public void onClick(View view) {
 
-                Intent intentMain3 = new Intent(v.getContext(), Main3Activity.class);
-                startActivity(intentMain3);
+                Tarea tarea = new Tarea();
+                tarea.setNombre(txtNombTarea.getText().toString());
+                tarea.setDescripcion(txtDescripTarea.getText().toString());
+                tarea.setTema(txtTemaTarea.getText().toString());
+                tarea.setComplejidad(sprComplejidad.getSelectedItem().toString());
+                tarea.setClasificacion(sprClasificacion.getSelectedItem().toString());
+                tarea.setArea(sprArea.getSelectedItem().toString());
+                tarea.setNombActividad(sprActividad.getSelectedItem().toString());
+                tarea.setFechaInicio("00/00/0000");
+                Calendar fechAsig = Calendar.getInstance();
+                int dia = fechAsig.get(Calendar.DAY_OF_MONTH);
+                int mes = fechAsig.get(Calendar.MONTH);
+                int anio = fechAsig.get(Calendar.YEAR);
+
+                tarea.setFechaAsignacion(dia+"/"+(mes+1)+"/"+anio);  //El dia que ingresa la tarea
+                tarea.setFechaEntrega(txtFechaEntrega.getText().toString());
+                tarea.setFechaFinalizacion("00/00/0000");
+
+                Toast.makeText(getApplicationContext(),txtNombTarea.getText().toString(), Toast.LENGTH_LONG).show();
+                myRef = FirebaseDatabase.getInstance().getReference().child("");
+                String key = myRef.push().getKey();
+                myRef = database.getReference(PATH_TAREAS + key);
+                tarea.setIdTarea(myRef.getKey());
+                myRef.setValue(tarea);
+
             }
         });
 
-         */
+    }
+
+    public void spinnerActiv(){
+
+        List<String> actividades = new ArrayList<>();
+        myRef.child("actividades").addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+
+                if(dataSnapshot.exists()){
+                    for(DataSnapshot ds: dataSnapshot.getChildren() ){
+
+                        String nombre = ds.child("nombre").getValue().toString();
+                        actividades.add(nombre);
+                    }
+
+                    ArrayAdapter<String> adapterAct = new ArrayAdapter<>(AgregarTareaActivity.this, android.R.layout.simple_dropdown_item_1line, actividades);
+                    sprActividad.setAdapter(adapterAct);
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
 
     }
 }
+
--- FocusAppM/app/src/main/java/com/example/focusappm/Tarea.java
+++ FocusAppM/app/src/main/java/com/example/focusappm/Tarea.java
@@ -1,8 +1,133 @@
 package com.example.focusappm;
 
+import java.util.Date;
+
 public class Tarea {
 
-    
+    private String idTarea;
+    private String nombre;
+    private String descripcion;
+    private String tema;
+    private String complejidad;
+    private String area;
+    private String clasificacion;
+    private Date fechaAsignacion;
+    private String fechaInicio;
+    private String fechaFinalizacion;
+    private Date fechaEntrega;
+    private String nombActividad;
+
+    public Tarea(){
+
+    }
+
+    public String getIdTarea() {
+        return idTarea;
+    }
+
+    public void setIdTarea(String idTarea) {
+        this.idTarea = idTarea;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public String getTema() {
+        return tema;
+    }
+
+    public void setTema(String tema) {
+        this.tema = tema;
+    }
+
+    public String getComplejidad() {
+        return complejidad;
+    }
+
+    public void setComplejidad(String complejidad) {
+        this.complejidad = complejidad;
+    }
+
+    public String getArea() {
+        return area;
+    }
+
+    public void setArea(String area) {
+        this.area = area;
+    }
+
+    public String getClasificacion() {
+        return clasificacion;
+    }
+
+    public void setClasificacion(String clasificacion) {
+        this.clasificacion = clasificacion;
+    }
+
+    public Date getFechaAsignacion() {
+        return fechaAsignacion;
+    }
+
+    /*
+    public void setFechaAsignacion(Date fechaAsignacion) {
+        this.fechaAsignacion = fechaAsignacion;
+    }
+     */
+
+    public void setFechaAsignacion(String s) {
+    }
+
+    public String getFechaInicio() {
+        return fechaInicio;
+    }
+
+    public void setFechaInicio(String fechaInicio) {
+        this.fechaInicio = fechaInicio;
+    }
+
+    public String getFechaFinalizacion() {
+
+        return fechaFinalizacion;
+    }
+
+    public void setFechaFinalizacion(String fechaFinalizacion) {
+        this.fechaFinalizacion = fechaFinalizacion;
+    }
+
+    public Date getFechaEntrega() {
+        return fechaEntrega;
+    }
+
+    public void setFechaEntrega(String s) {
+        this.fechaEntrega = fechaEntrega;
+    }
 
 
+    /*
+    public void setFechaEntrega(Date fechaEntrega) {
+        this.fechaEntrega = fechaEntrega;
+    }
+     */
+
+    public String getNombActividad() {
+        return nombActividad;
+    }
+
+    public void setNombActividad(String nombActividad) {
+        this.nombActividad = nombActividad;
+    }
 }
+
