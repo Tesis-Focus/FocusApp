0c89d75bcc3ef6c22634c150a0e5667d49da6c92
JuanPabl0770 <53346852+JuanPabl0770@users.noreply.github.com>
2020-05-07 10:43:37 -0500

    Calendario zoom dias

--- FocusAppM/app/src/main/java/com/example/focusappm/MesFragment.java
+++ FocusAppM/app/src/main/java/com/example/focusappm/MesFragment.java
@@ -7,6 +7,7 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CalendarView;
 
 
 /**
@@ -18,11 +19,15 @@ public class MesFragment extends Fragment {
         // Required empty public constructor
     }
 
+    CalendarView mCalendarView;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
+        View rootView = inflater.inflate(R.layout.fragment_semana,container,false);
+        mCalendarView = rootView.findViewById(R.id.calendarView);
+
         return inflater.inflate(R.layout.fragment_mes, container, false);
     }
 }
