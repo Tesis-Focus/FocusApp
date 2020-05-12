013de9c6d11a7eacd824280669037cd85fa7edd0
Lina Marcela Varón <31346699+linamvaron97@users.noreply.github.com>
2020-05-12 11:55:19 -0500

    Instrucciones test

--- FocusAppM/app/src/main/java/com/example/focusappm/AgregarPerfilActivity.java
+++ FocusAppM/app/src/main/java/com/example/focusappm/AgregarPerfilActivity.java
@@ -216,4 +216,3 @@ public class AgregarPerfilActivity extends AppCompatActivity {
             return posicion;
         }
 }
-
---
+++ FocusAppM/app/src/main/java/com/example/focusappm/InstruccionesTest.java
@@ -0,0 +1,49 @@
+package com.example.focusappm;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.MediaController;
+import android.widget.VideoView;
+
+public class InstruccionesTest extends AppCompatActivity {
+
+    Button btnEmpezar;
+    VideoView videoView;
+    Usuario beneficiario;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_instrucciones_test);
+
+        beneficiario = (Usuario) getIntent().getSerializableExtra("beneficiario");
+        btnEmpezar = findViewById(R.id.btnEmpezar);
+        /*
+        videoView = findViewById(R.id.videoView);
+
+        String videoPath = "android.resource://" + getPackageName() + "/" + R.raw.ejemplo_test;
+        Uri uri = Uri.parse(videoPath);
+        videoView.setVideoURI(uri);
+
+        MediaController mediaController = new MediaController(this);
+        videoView.setMediaController(mediaController);
+        mediaController.setAnchorView(videoView);
+
+         */
+
+        btnEmpezar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent intent = new Intent(getBaseContext(), TestAprendizajeActivity.class);
+                intent.putExtra("beneficiario", beneficiario);
+                startActivity(intent);
+            }
+        });
+    }
+}
