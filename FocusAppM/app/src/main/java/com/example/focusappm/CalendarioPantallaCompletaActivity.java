390884c168efd3ab46bd38bc3b45f7b397b2b080
JuanPabl0770 <53346852+JuanPabl0770@users.noreply.github.com>
2020-05-12 22:51:10 -0500

    fechas arregladas

---
+++ FocusAppM/app/src/main/java/com/example/focusappm/CalendarioPantallaCompletaActivity.java
@@ -0,0 +1,14 @@
+package com.example.focusappm;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CalendarioPantallaCompletaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_calendario_pantalla_completa);
+    }
+}
